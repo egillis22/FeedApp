6794da581d25da7096ba8b50cd92e84bc4a9ff0f
Erica Gillis <erica_gillis1@baylor.edu>
2021-04-24 16:58:05 -0500

    added profile to model

--- FeedApp/models.py
+++ FeedApp/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length = 200, blank = True)
+    last_name = models.CharField(max_length = 200, blank = True)
+    email = models.EmailField(max_length=300, blank = True)
+    dob = models.DateField(null = True, blank=True)
+    bio = models.TextField(blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    friends = moedls.ManyToManyField(User, blank=True, related_name = 'friends')
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}--{self.created}"
+
+
 STATUS_CHOICES = (
     ('send','send'),
     ('accepted', 'accepted')
